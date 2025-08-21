36cdc8e171ebe397b2d52f8838ba76f811400f10
BijanProgrammer <bijaneisapour@gmail.com>
2025-08-21 12:50:49 +0330

    [ADD] global styles

--- src/App.tsx
+++ src/App.tsx
@@ -3,5 +3,11 @@ import { type ReactNode } from "react";
 import "./App.css";
 
 export default function App(): ReactNode {
-  return <div className="app">Hello, friend!</div>;
+  return (
+    <div className="app">
+      <header>Header</header>
+      <main>Hello, friend!</main>
+      <footer>Footer</footer>
+    </div>
+  );
 }
