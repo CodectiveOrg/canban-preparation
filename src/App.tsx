bef471fcedfffd1f63d3459d61cb03ec9994f28a
BijanProgrammer <bijaneisapour@gmail.com>
2025-08-21 13:30:31 +0330

    [ADD] boards

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,44 @@ export default function App(): ReactNode {
   return (
     <div className="app">
       <header>Header</header>
-      <main>Hello, friend!</main>
+      <main>
+        <h1>Boards</h1>
+        <ul>
+          <li>
+            <div className="board">
+              <div className="header">
+                <div className="title">Board 1</div>
+                <a href="/board/1">View</a>
+              </div>
+              <p className="description">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                Aperiam, at.
+              </p>
+            </div>
+          </li>
+          <li>
+            <div className="board">
+              <div className="header">
+                <div className="title">Board 2</div>
+                <a href="/board/2">View</a>
+              </div>
+              <p className="description">
+                Corporis maxime officia quo sunt? Ad blanditiis consectetur
+                consequuntur dicta enim error eveniet explicabo ipsa ipsum
+              </p>
+            </div>
+          </li>
+          <li>
+            <div className="board">
+              <div className="header">
+                <div className="title">Board 3</div>
+                <a href="/board/3">View</a>
+              </div>
+              <p className="description">doloribus ex id natus vel!</p>
+            </div>
+          </li>
+        </ul>
+      </main>
       <footer>Footer</footer>
     </div>
   );
