94df90c725cb6e5646c20d86f621e45e43a883ee
BijanProgrammer <bijaneisapour@gmail.com>
2025-08-28 09:57:49 +0330

    [ADD] move logic

--- src/components/Board/Board.tsx
+++ src/components/Board/Board.tsx
@@ -1,5 +1,8 @@
 import { type ReactNode, useCallback, useState } from "react";
 
+import { listsData } from "@/data/lists-data.ts";
+
+import Button from "@/components/Button/Button.tsx";
 import IconButton from "@/components/IconButton/IconButton.tsx";
 import List from "@/components/List/List.tsx";
 
@@ -11,52 +14,83 @@ import type { ListType } from "@/types/list.ts";
 import styles from "./Board.module.css";
 
 export default function Board(): ReactNode {
-  const [todoList, setTodoList] = useState<ListType>({
-    id: "1",
-    title: "🔜 To Do",
-    items: [
-      { id: "1", title: "Setup Backend Project" },
-      { id: "2", title: "Find a Good Name for the Project" },
-      { id: "3", title: "Implement Landing Page" },
-    ],
-  });
+  const [lists, setLists] = useState<ListType[]>(listsData);
 
-  const [doingList] = useState<ListType>({
-    id: "2",
-    title: "🔨 Doing",
-    items: [
-      { id: "4", title: "Setup Frontend Project" },
-      { id: "5", title: "Design Landing Page" },
-    ],
-  });
+  const [activeListId, setActiveListId] = useState<string | null>(null);
+  const [activeItemId, setActiveItemId] = useState<string | null>(null);
 
-  const [doneList] = useState<ListType>({
-    id: "3",
-    title: "🎉 Done",
-    items: [],
-  });
+  const handleListItemClick = useCallback(
+    (listId: string, itemId: string): void => {
+      setActiveListId(listId);
+      setActiveItemId(itemId);
+    },
+    [],
+  );
 
-  const handleEditButtonClick = (): void => {
-    setTodoList((old) => {
-      const clone = [...old.items];
-      clone.splice(1, 1);
-      return { ...old, items: clone };
+  const handleMoveButtonClick = (listId: string): void => {
+    setLists((old) => {
+      try {
+        const activeListIndex = old.findIndex(
+          (list) => list.id === activeListId,
+        );
+        const destinationListIndex = old.findIndex(
+          (list) => list.id === listId,
+        );
+
+        if (activeListIndex === -1 || destinationListIndex === -1) {
+          console.error("Cannot find desired lists.");
+          return old;
+        }
+
+        const clone = [...old];
+        const activeList = { ...clone[activeListIndex] };
+        const destinationList = { ...clone[destinationListIndex] };
+
+        const activeItemIndex = activeList.items.findIndex(
+          (item) => item.id === activeItemId,
+        );
+
+        if (activeItemIndex === -1) {
+          console.error("Cannot find desired item.");
+          return old;
+        }
+
+        const [activeItem] = activeList.items.splice(activeItemIndex, 1);
+        destinationList.items.push(activeItem);
+
+        clone[activeListIndex] = activeList;
+        clone[destinationListIndex] = destinationList;
+        return clone;
+      } finally {
+        setActiveListId(null);
+        setActiveItemId(null);
+      }
     });
   };
 
-  const handleListItemClick = useCallback((id: string): void => {
-    setTodoList((old) => ({
-      ...old,
-      items: old.items.filter((item) => item.id !== id),
-    }));
-  }, []);
+  const handleRemoveButtonClick = (): void => {};
 
   return (
     <div className={styles.board}>
       <div className={styles.toolbar}>
         <div className={styles.title}>Board Title</div>
         <div className={styles.actions}>
-          <IconButton onClick={handleEditButtonClick}>
+          {activeListId !== null && (
+            <div className={styles.spacer}>
+              {lists
+                .filter((list) => list.id !== activeListId)
+                .map((list) => (
+                  <Button
+                    key={list.id}
+                    onClick={() => handleMoveButtonClick(list.id)}
+                  >
+                    {list.title}
+                  </Button>
+                ))}
+              <Button onClick={handleRemoveButtonClick}>Remove</Button>
+            </div>
+          )}
+          <IconButton>
             <MingcuteEdit2Line />
           </IconButton>
           <IconButton>
@@ -65,15 +99,11 @@ export default function Board(): ReactNode {
         </div>
       </div>
       <ul className={styles.lists}>
-        <li>
-          <List list={todoList} onClick={handleListItemClick} />
-        </li>
-        <li>
-          <List list={doingList} onClick={handleListItemClick} />
-        </li>
-        <li>
-          <List list={doneList} onClick={handleListItemClick} />
-        </li>
+        {lists.map((list) => (
+          <li key={list.id}>
+            <List list={list} onClick={handleListItemClick} />
+          </li>
+        ))}
       </ul>
     </div>
   );
