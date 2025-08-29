4141f9b05592b39fecef7f8552c871e35e81a957
BijanProgrammer <bijaneisapour@gmail.com>
2025-08-29 11:22:30 +0330

    [ADD] persist data using localStorage

--- src/components/Board/Board.tsx
+++ src/components/Board/Board.tsx
@@ -1,7 +1,5 @@
 import { type ReactNode, useCallback, useMemo, useState } from "react";
 
-import { listsData } from "@/data/lists-data.ts";
-
 import Button from "@/components/Button/Button.tsx";
 import IconButton from "@/components/IconButton/IconButton.tsx";
 import List from "@/components/List/List.tsx";
@@ -13,8 +11,21 @@ import type { ListType } from "@/types/list.ts";
 
 import styles from "./Board.module.css";
 
+function save(lists: ListType[]): void {
+  localStorage.setItem("lists", JSON.stringify(lists));
+}
+
+function load(): ListType[] {
+  const item = localStorage.getItem("lists");
+  if (!item) {
+    return [];
+  }
+
+  return JSON.parse(item);
+}
+
 export default function Board(): ReactNode {
-  const [lists, setLists] = useState<ListType[]>(listsData);
+  const [lists, setLists] = useState<ListType[]>(load);
 
   const [activeListId, setActiveListId] = useState<string | null>(null);
   const [activeItemId, setActiveItemId] = useState<string | null>(null);
@@ -34,6 +45,7 @@ export default function Board(): ReactNode {
       const id = globalThis.crypto.randomUUID();
       clone[0] = { ...clone[0], items: [...clone[0].items, { id, title: id }] };
 
+      save(clone);
       return clone;
     });
   };
@@ -78,6 +90,7 @@ export default function Board(): ReactNode {
 
           clone[activeListIndex] = activeList;
           clone[destinationListIndex] = destinationList;
+          save(clone);
           return clone;
         } finally {
           setActiveListId(null);
@@ -118,6 +131,7 @@ export default function Board(): ReactNode {
         activeList.items.splice(activeItemIndex, 1);
 
         clone[activeListIndex] = activeList;
+        save(clone);
         return clone;
       } finally {
         setActiveListId(null);
