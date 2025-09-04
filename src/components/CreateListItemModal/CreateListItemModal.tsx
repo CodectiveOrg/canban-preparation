8c533096ebaea8221ff00e4378011467f14c884c
BijanProgrammer <bijaneisapour@gmail.com>
2025-09-04 05:00:33 +0330

    [ADD] submit logic

--- src/components/CreateListItemModal/CreateListItemModal.tsx
+++ src/components/CreateListItemModal/CreateListItemModal.tsx
@@ -1,4 +1,9 @@
-import { type ComponentProps, type FormEvent, type ReactNode } from "react";
+import {
+  type ComponentProps,
+  type FormEvent,
+  type ReactNode,
+  useContext,
+} from "react";
 
 import clsx from "clsx";
 
@@ -6,21 +11,42 @@ import Button from "@/components/Button/Button.tsx";
 import Modal from "@/components/Modal/Modal.tsx";
 import TextInputComponent from "@/components/text-input/text-input.component.tsx";
 
+import { BoardContext } from "@/context/board-context.ts";
+
 import styles from "./CreateListItemModal.module.css";
 
-type Props = Omit<ComponentProps<typeof Modal>, "heading">;
+type Props = Omit<ComponentProps<typeof Modal>, "heading"> & {
+  listId: string | null;
+};
 
 export default function CreateListItemModal({
   ref,
   contentClassName,
+  listId,
   ...otherProps
 }: Props): ReactNode {
+  const { create } = useContext(BoardContext);
+
   const handleCancelButtonClick = (): void => {
     ref.current?.close();
   };
 
   const handleFormSubmit = (e: FormEvent<HTMLFormElement>): void => {
     e.preventDefault();
+
+    if (!listId) {
+      console.error("Cannot find desired list.");
+      return;
+    }
+
+    const formData = new FormData(e.currentTarget);
+    const id = globalThis.crypto.randomUUID();
+    const title = formData.get("title") as string;
+
+    create(listId, { id, title });
+
+    e.currentTarget.reset();
+    ref.current?.close();
   };
 
   return (
@@ -34,7 +60,7 @@ export default function CreateListItemModal({
       {...otherProps}
     >
       <form onSubmit={handleFormSubmit}>
-        <TextInputComponent label="Title" />
+        <TextInputComponent label="Title" name="title" />
         <div className={styles.actions}>
           <Button type="button" onClick={handleCancelButtonClick}>
             Cancel
--- src/pages/BoardPage/BoardPage.tsx
+++ src/pages/BoardPage/BoardPage.tsx
@@ -1,5 +1,6 @@
-import { type ReactNode, useRef } from "react";
+import { type ReactNode, useRef, useState } from "react";
 
+import Board from "@/components/Board/Board.tsx";
 import Button from "@/components/Button/Button.tsx";
 import CreateListItemModal from "@/components/CreateListItemModal/CreateListItemModal.tsx";
 
@@ -9,24 +10,35 @@ import BoardProvider from "@/providers/BoardProvider.tsx";
 import styles from "./BoardPage.module.css";
 
 export default function BoardPage(): ReactNode {
+  const [modalListId, setModalListId] = useState<string | null>(null);
+
   const modalRef = useRef<HTMLDialogElement>(null);
 
-  const handleOpenButtonClick = (): void => {
+  const handleOpenButtonClick = (listId: string): void => {
+    setModalListId(listId);
     modalRef.current?.showModal();
+  };
+
+  const handleModalClose = (): void => {
+    setModalListId(null);
   };
 
   return (
     <BoardProvider>
       <ActiveItemProvider>
         <div className={styles["board-page"]}>
-          {/*<Board />*/}
-
-          <Button color="primary" onClick={handleOpenButtonClick}>
+          <Button color="primary" onClick={() => handleOpenButtonClick("1")}>
             Open
           </Button>
-          <CreateListItemModal ref={modalRef}>
+          <CreateListItemModal
+            ref={modalRef}
+            listId={modalListId}
+            onClose={handleModalClose}
+          >
             This is content.
           </CreateListItemModal>
+
+          <Board />
         </div>
       </ActiveItemProvider>
     </BoardProvider>
