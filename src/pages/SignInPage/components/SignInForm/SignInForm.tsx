9509dd1896d87c53198397950d0fdc7672ab7b3f
BijanProgrammer <bijaneisapour@gmail.com>
2026-08-21 11:40:39 +0330

    [ADD] sign in api call

--- src/pages/SignInPage/components/SignInForm/SignInForm.tsx
+++ src/pages/SignInPage/components/SignInForm/SignInForm.tsx
@@ -1,5 +1,7 @@
 import type { ReactNode } from "react";
 
+import { useNavigate } from "react-router";
+
 import { toast } from "react-toastify";
 
 import { zodResolver } from "@hookform/resolvers/zod";
@@ -9,6 +11,8 @@ import { z } from "zod";
 import Button from "@/components/Button/Button.tsx";
 import TextInput from "@/components/TextInput/TextInput.tsx";
 
+import type { ResponseDto } from "@/dto/response.dto.ts";
+
 import { SignInSchema } from "@/schemas/sign-in-schema.ts";
 
 import styles from "./SignInForm.module.css";
@@ -16,6 +20,8 @@ import styles from "./SignInForm.module.css";
 type Values = z.infer<typeof SignInSchema>;
 
 export default function SignInForm(): ReactNode {
+  const navigate = useNavigate();
+
   const {
     register,
     handleSubmit,
@@ -24,9 +30,30 @@ export default function SignInForm(): ReactNode {
     resolver: zodResolver(SignInSchema),
   });
 
-  const handleFormSubmit = (values: Values): void => {
-    console.log(values);
-    toast.success("Signed in successfully.");
+  const handleFormSubmit = async (values: Values): Promise<void> => {
+    try {
+      const response = await fetch("https://api.canban.ir/auth/sign-in", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(values),
+        credentials: "include",
+      });
+
+      const data = (await response.json()) as ResponseDto;
+
+      if ("error" in data) {
+        toast.error(data.message);
+        return;
+      }
+
+      toast.success("Signed in successfully.");
+      navigate("/");
+    } catch (error) {
+      console.error(error);
+      toast.error("Something went wrong! Please try again later.");
+    }
   };
 
   return (
